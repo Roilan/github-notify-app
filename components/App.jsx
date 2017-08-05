61b26485c4b0acc71fc14184d25166cddb7b5a28
Roilan <roilan14@gmail.com>
2017-08-05 11:13:15 -0400

    es6ify

--- components/App.jsx
+++ components/App.jsx
@@ -1,14 +1,18 @@
-const React = require('react');
+import React, { Component } from 'react';
+import injectTapEvent from 'react-tap-event-plugin';
+import ThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Login from './Login';
 
-class App extends React.Component {
+injectTapEvent();
+
+class App extends Component {
   render() {
     return (
-      <div>
-        <h1>tester</h1>
-        Hello React
-      </div>
+      <ThemeProvider>
+        <Login />
+      </ThemeProvider>
     );
   }
 }
 
-module.exports = App;
+export default App;
