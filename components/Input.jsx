85f51706ce7809287a65f91c4ea9f1a1514a746b
Roilan <roilan14@gmail.com>
2017-08-05 11:36:01 -0400

    fullwidth prop

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
 
-const Input = ({ id, label, errorText, onChange, value }) => (
+const Input = ({
+  id, label, errorText, onChange, value,
+  fullWidth
+}) => (
   <TextField
     id={id}
     floatingLabelText={label}
+    fullWidth={fullWidth}
     errorText={errorText}
     onChange={onChange}
     value={value}
