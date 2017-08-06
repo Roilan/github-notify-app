37601788fce1d2db623417013e02b453645589c3
Roilan <roilan14@gmail.com>
2017-08-05 22:11:24 -0400

    home-made promisify due to bluebird issues'

--- utils/storage.js
+++ utils/storage.js
@@ -1,6 +1,17 @@
-import Bluebird from 'bluebird';
 import storage from 'electron-json-storage';
 
-const promisifyStorage = Bluebird.promisifyAll(storage);
+const promisify = (func) => new Promise((resolve, reject) => {
+  func((error, data) => (
+    error ? reject(error) : resolve(data)
+  ))
+});
 
-export default promisifyStorage;
+const get = key => promisify(storage.get.bind(null, key))
+const getAll = () => promisify(storage.getAll);
+const set = (key, json) => promisify(storage.set.bind(null, key, json));
+
+export default {
+  get,
+  getAll,
+  set
+}
